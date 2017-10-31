e75d974ac0876f5159dc29db70e28b8b298b8927
Alexandre Miranda <alexandre.mm@outlook.com>
2017-10-31 10:46:58 -0200

    Double jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -37,6 +37,10 @@ public class PlayerController : MonoBehaviour {
 
 	public LayerMask whatIsGround;
 
+	bool canDoubleJump = false;
+
+	public float delayForDoubleJump = 0.2f;
+
 	Animator anim;
 	// Use this for initialization
 	void Start () {
@@ -102,14 +106,27 @@ public class PlayerController : MonoBehaviour {
 			isJumping = true;
 		rb.AddForce(new Vector2(0f, jumpSpeed));
 		anim.SetInteger("State", 1);
+
+		Invoke("EnableDoubleJump", delayForDoubleJump);
+		}
+		if (canDoubleJump && !isGrounded){
+		rb.velocity = Vector2.zero;
+		rb.AddForce(new Vector2(0f, jumpSpeed));
+		anim.SetInteger("State", 1);
+		canDoubleJump = false;
+		}
 	}
-}
+
+	void EnableDoubleJump(){
+		canDoubleJump = true;
+	}
+
 	void OnCollisionEnter2D(Collision2D other){
 		if (other.gameObject.layer == LayerMask.NameToLayer("Ground")){
 			isJumping = false;
 		}
 		else {
-			isJumping = true;
+			isJumping = true;			
 		}
 	}
 }
