3807ed0f3ef761635d6d8a093827dab01a09e9c8
Alexandre Miranda <alexandre.mm@outlook.com>
2017-12-05 11:21:34 -0200

    Tocando efeito fail e controlando musica

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -25,8 +25,8 @@ public class AudioManager : MonoBehaviour {
 		AudioSource.PlayClipAtPoint(audioPlayer.jump, obj.transform.position);
 	}
 	
-	
-	
-
+	public void PlayFailSound(GameObject obj){
+	AudioSource.PlayClipAtPoint(audioSFX.fail, obj.transform.position);	
+	}
 	
 }
